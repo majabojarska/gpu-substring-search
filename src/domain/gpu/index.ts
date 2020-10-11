fff50e469f5359d940f7331f8a83f264ca7f329b
damiankoper <kopernickk@gmail.com>
2020-10-11 17:19:53 +0200

    Webpack setup

---
+++ src/domain/gpu/index.ts
@@ -0,0 +1,10 @@
+import { GPU } from "gpu.js";
+
+const gpu = new GPU();
+const kernel = gpu.createKernel(
+  function () {
+    return this.thread.x;
+  },
+  { output: [100] }
+);
+console.log(kernel());
---
+++ src/index.ts
@@ -0,0 +1,5 @@
+import "./domain/benchmark";
+import "./domain/gui";
+import "./domain/gpu";
+
+console.log("Hello World from your main file!");
