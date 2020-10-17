daf10dcac9fae62fcf84f404f0a9d011da9b7fb8
damiankoper <kopernickk@gmail.com>
2020-10-17 13:54:44 +0200

    React components

--- src/domain/gui/components/HelloJakub.tsx
+++ src/domain/gui/components/HelloJakub.tsx
@@ -2,10 +2,5 @@ import React from "react";
 import { Button } from "@material-ui/core";
 
 export default function HelloJakub(): React.ReactElement {
-  return (
-    <div>
-      Hello Jakobinie <br />
-      <Button variant="contained">XD</Button>
-    </div>
-  );
+  return <div>Hello Jakub</div>;
 }
