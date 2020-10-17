5e9ea4915d886b4dc5a9d05fa50b4170b5cafa8c
damiankoper <kopernickk@gmail.com>
2020-10-17 13:55:17 +0200

    CPU Workers

--- src/domain/cpu/index.ts
+++ src/domain/cpu/index.ts
@@ -1,5 +1,25 @@
 import Worker from "worker-loader!./workers/hello.worker";
-
+import { DataInMessagePayload } from "./workers/hello.worker";
 const worker = new Worker();
 
-worker.postMessage("Hello from worker");
+import { DataSet, DataProvider } from "../data/index";
+const decoder = new TextDecoder();
+const textLen = 1000,
+  patternLen = 5;
+const dataProvider = new DataProvider(textLen, patternLen);
+const dataSet: DataSet = dataProvider.getData();
+
+const dataIn: DataInMessagePayload = {
+  offset: 0,
+  chunkLength: textLen,
+  pattern: dataSet.pattern,
+  text: dataSet.text,
+};
+console.log(`Text: ${decoder.decode(dataIn.text)}`);
+console.log(`Pattern: ${decoder.decode(dataIn.pattern)}`);
+
+worker.postMessage(dataIn);
+
+worker.onmessage = (message) => {
+  console.log(`Found matching indices: ${message.data.matches}`);
+};
--- src/domain/cpu/workers/hello.worker.ts
+++ src/domain/cpu/workers/hello.worker.ts
@@ -1,6 +1,63 @@
-const ctx: Worker = (self as unknown) as Worker;
-ctx.onmessage = (message: MessageEvent) => {
-  console.log(message);
-};
+export interface DataInMessagePayload {
+  offset: number;
+  chunkLength: number;
+  pattern: Uint8Array;
+  text: Uint8Array;
+}
 
+export interface DataOutMessagePayload {
+  matches: Array<number>;
+}
+
+class SolverCPU {
+  private readonly ctx: Worker = (self as unknown) as Worker;
+
+  constructor() {
+    this.ctx.onmessage = this.handleMessage.bind(this);
+  }
+
+  handleMessage(message: MessageEvent<DataInMessagePayload>) {
+    if (message.data.pattern.length < 1 || message.data.text.length < 1) {
+      throw Error("Pattern and text must be at least 1 character long.");
+    }
+    if (message.data.pattern.length > message.data.text.length) {
+      throw Error("Pattern can't be longer than text.");
+    }
+    const matches = this.solve(message.data);
+    this.ctx.postMessage({
+      matches: matches,
+    });
+  }
+
+  solve(data: DataInMessagePayload): Array<number> {
+    const foundMatches: Array<number> = [];
+
+    // Maximum valid index to start pattern matching from.
+    const maxTextIdx = Math.min(
+      data.offset + data.chunkLength - 1,
+      data.text.length - data.pattern.length
+    );
+    for (let textIdx = data.offset; textIdx <= maxTextIdx; ++textIdx) {
+      let notMatching = false;
+
+      // Compare pattern with text substring at textIdx.
+      for (let patternIdx = 0; patternIdx < data.pattern.length; ++patternIdx) {
+        const currentTextIdx = textIdx + patternIdx;
+        if (data.text[currentTextIdx] != data.pattern[patternIdx]) {
+          // At least one character not matching => No match at textIdx.
+          notMatching = true;
+          break;
+        }
+      }
+      if (notMatching) {
+        continue;
+      }
+      foundMatches.push(textIdx);
+    }
+
+    return foundMatches;
+  }
+}
+
+new SolverCPU();
 export default null as unknown;
