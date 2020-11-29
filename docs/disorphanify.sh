ef749c1ec888022172732d08509a9f2081dddb25
Maja Bojarska <33836570+majabojarska@users.noreply.github.com>
2020-11-29 18:18:43 +0100

    Documentation (#11)

---
+++ docs/disorphanify.sh
@@ -0,0 +1,10 @@
+find $(dirname $0)/parts -regex '.*\(tex\)$' -exec sed -i -e 's/ i / i~/g' {} \;
+find $(dirname $0)/parts -regex '.*\(tex\)$' -exec sed -i -e 's/ a / a~/g' {} \;
+find $(dirname $0)/parts -regex '.*\(tex\)$' -exec sed -i -e 's/ o / o~/g' {} \;
+find $(dirname $0)/parts -regex '.*\(tex\)$' -exec sed -i -e 's/ u / u~/g' {} \;
+find $(dirname $0)/parts -regex '.*\(tex\)$' -exec sed -i -e 's/ w / w~/g' {} \;
+find $(dirname $0)/parts -regex '.*\(tex\)$' -exec sed -i -e 's/ z / z~/g' {} \;
+find $(dirname $0)/parts -regex '.*\(tex\)$' -exec sed -i -e 's/ z / z~/g' {} \;
+find $(dirname $0)/parts -regex '.*\(tex\)$' -exec sed -i -e 's/ Z / Z~/g' {} \;
+find $(dirname $0)/parts -regex '.*\(tex\)$' -exec sed -i -e 's/ A / A~/g' {} \;
+find $(dirname $0)/parts -regex '.*\(tex\)$' -exec sed -i -e 's/ W / W~/g' {} \;
--- src/domain/gpu/Solver.ts
+++ src/domain/gpu/Solver.ts
@@ -6,10 +6,7 @@ import {
   KernelOutput,
   IKernelRunShortcut,
 } from "gpu.js";
-import {
-  DataInMessagePayload,
-  DataOutMessagePayload,
-} from "./Payloads";
+import { DataInMessagePayload, DataOutMessagePayload } from "./Payloads";
 
 export default class SolverGPU {
   private gpu: GPU;
